eb91ac7f8897a0511a89a1bc1af40473c7955369
Billy Larner <billy.larner@students.plymouth.ac.uk>
2024-09-26 13:54:17 +0100

    test
    Signed-off-by: Billy Larner <billy.larner@students.plymouth.ac.uk>

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()                                                  // creates an interger called main
+{   
+    printf("Hello World\n");                                // prints Hello World
+
+    int a;                                                  // creates and interger called a
+
+    for (a=0; a < 5; a = a + 1)                             // a is euqal to 0, while a is less than five add one to a
+    {
+        printf("a = %d\n",a);                               // prints a = then the interger a
+
+        //test
+    }
+    
+    printf("final value of a is %d\n", a);                  // prints final value of a is interger a
+    
+    while (true);                                           
+}
